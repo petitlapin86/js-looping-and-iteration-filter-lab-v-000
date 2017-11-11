09bbf567ba8993a1bb6efd01c688956bd74e40ed
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-11 23:02:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ function findMatching(drivers){
   return drivers === ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby';]
 }
 
+function fuzzyMatch (list, partialName) {
+  let lengthOfName = partialName.length;
+  return list.filter(function (driverName) {
+    return driverName.slice(0, lengthOfName) === partialName;
+  });
+}
+
+function matchName (list, name) {
+  return list.filter(function (driver) {
+    return driver.name.toLowerCase() === name.toLowerCase();
+  });
+}
